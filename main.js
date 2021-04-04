18bf1ce4e99cf310199f83662a2690bbcd16e509
Sawyer Carlton <skcarlton@gmail.com>
2021-04-04 18:46:51 -0400

    Make array

---
+++ main.js
@@ -0,0 +1,1 @@
+const musicFests = ['Electric Daisy Carnival Orlando', 'Sunset Music Festival', 'Moonrise Festival', 'Suwannee Hulaween', 'BUKU Music + Art Project', 'Okeechobee Music & Arts Festival', 'Ultra Music Festival', 'Home Bass Orlando', 'HiJinx Festival', 'Imagine Music Festival'];
