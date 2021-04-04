345ac1f9aaf3e0bb75de3937633169127f35de0a
Sawyer Carlton <skcarlton@gmail.com>
2021-04-04 19:37:33 -0400

    Add button and functionality for randomization

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 const musicFests = ['Electric Daisy Carnival Orlando', 'Sunset Music Festival', 'Moonrise Festival', 'Suwannee Hulaween', 'BUKU Music + Art Project', 'Okeechobee Music & Arts Festival', 'Ultra Music Festival', 'Home Bass Orlando', 'HiJinx Festival', 'Imagine Music Festival'];
+
+const randomFest = (festivals) => {
+    let currentIndex = festivals.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -=1;
+
+        temporaryValue = festivals[currentIndex];
+        festivals[currentIndex] = festivals[randomIndex];
+        festivals[randomIndex] = temporaryValue;
+    }
+
+    return festivals;
+};
+
+const pickFest = () => {
+    document.getElementById('fest').innerHTML = randomFest(musicFests)[0];
+};
